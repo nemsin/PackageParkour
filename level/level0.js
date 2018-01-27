b40866a49c2a9dd9f3129852e575a5a407dc7b6e
Nemsin <nemsinsc2@gmail.com>
2018-01-28 00:57:18 +0100

    rename obstacle to step in level

--- level/level0.js
+++ level/level0.js
@@ -9,16 +9,16 @@ function buildWorld0()
     //plates[0][0] = game.add.sprite(200, 545, 'plate');
     //plates[0][1] = false;
 
-    //liftable obstacles require physics!!
-    obstacles[1] = [];
-    obstacles[1][0] = grounds.create(250, 550, 'ground');
-    //obstacles[1][0].scale.setTo(0.1, 1);
-    game.physics.enable(obstacles[1][0], Phaser.Physics.ARCADE);
+    //liftable steps require physics!!
+    steps[1] = [];
+    steps[1][0] = grounds.create(250, 550, 'ground');
+    //steps[1][0].scale.setTo(0.1, 1);
+    game.physics.enable(steps[1][0], Phaser.Physics.ARCADE);
 
     // Ground Test
-    obstacles[0] = [];
-    obstacles[0][0] = grounds.create(300, 475, 'ground');
-    obstacles[0][0].scale.setTo(1, 1.5);
+    steps[0] = [];
+    steps[0][0] = grounds.create(300, 475, 'ground');
+    steps[0][0].scale.setTo(1, 1.5);
 
     // Finish
     finish = game.add.sprite(1000, 450, 'finish');
@@ -33,8 +33,8 @@ function leverHandler0()
         switch(triggerIndex)
         {
             case (0):
-                obstacles[1][1] = obstacles[1][0].y - 40;
-                obstacles[1][0].body.velocity.y = -10;
+                steps[1][1] = steps[1][0].y - 40;
+                steps[1][0].body.velocity.y = -10;
         }
     }
 }
@@ -54,13 +54,13 @@ function plateHandler0()
 
 function liftObstacle0()
 {
-    for (var i = 0; i < obstacles.length; i++)
+    for (var i = 0; i < steps.length; i++)
     {
-        if (obstacles[i][1] != null)
-            if (obstacles[i][0].y <= obstacles[i][1])
+        if (steps[i][1] != null)
+            if (steps[i][0].y <= steps[i][1])
             {
-                obstacles[i][1] = null;
-                obstacles[i][0].body.velocity.y = 0;
+                steps[i][1] = null;
+                steps[i][0].body.velocity.y = 0;
             }
     }
 }
@@ -68,8 +68,8 @@ function liftObstacle0()
 function finishHandler0()
 {
     level = 0;
-    obstacles[0][0].destroy();
-    obstacles[1][0].destroy();
+    steps[0][0].destroy();
+    steps[1][0].destroy();
     levers[0][0].destroy();
     player.x = 0;
     player.y = 0;
@@ -82,10 +82,10 @@ function finishHandler0()
 
 function reset0()
 {
-    obstacles[1][0].x = 250;
-    obstacles[1][0].y = 550;
-    obstacles[1][0].body.velocity.y = 0;
-    obstacles[1][1] = null;
+    steps[1][0].x = 250;
+    steps[1][0].y = 550;
+    steps[1][0].body.velocity.y = 0;
+    steps[1][1] = null;
 
     levers[0][1] = false;
 
