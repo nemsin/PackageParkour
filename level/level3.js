b736b8e78acfa65335d63e331f9c0a34531c717d
Nemsin <nemsinsc2@gmail.com>
2018-01-28 15:48:02 +0100

    add level3

--- level/level3.js
+++ level/level3.js
@@ -1,18 +1,33 @@
 function buildWorld3()
 {
     // Triggers
-    levers[0] = [];
-    levers[0][0] = game.add.sprite(525, 525, 'lever');
-    levers[0][1] = false;
-
     plates[0] = [];
-    plates[0][0] = game.add.sprite(1503, 525, 'plate');
+    plates[0][0] = game.add.sprite(250, 545, 'plate');
     plates[0][0].scale.setTo(3, 1);
     plates[0][1] = false;
 
+    plates[1] = [];
+    plates[1][0] = game.add.sprite(250, 545, 'plate');
+    plates[1][0].scale.setTo(3, 1);
+    plates[1][1] = false;
+
+    plates[2] = [];
+    plates[2][0] = game.add.sprite(620, 470, 'plate');
+    plates[2][0].scale.setTo(3, 1);
+    plates[2][1] = false;
+    
+    // First step
+    steps[0] = [];
+    steps[0][0] = grounds.create(600, 475, 'ground');
+    steps[0][0].scale.setTo(1, 1.5);
+
     // Wall
-    obstacles[0] = ground.create(200, 425, 'wall30');
-    obstacles[0].scale.setTo(0.01, 1.5);
+    obstacles[0] = grounds.create(200, 425, 'wall30');
+    obstacles[0].scale.setTo(1, 1.5);
+    obstacles[1] = grounds.create(500, 400, 'wall10');
+    obstacles[1].scale.setTo(1, 1);
+    obstacles[2] = grounds.create(500, 600, 'wall10');
+    obstacles[2].scale.setTo(1, 1);
 
     // Finish
     finish = game.add.sprite(2475, 375, 'finish');
@@ -41,8 +56,11 @@ function plateHandler3()
         switch(triggerIndex)
         {
             case (0):
-                steps[2][1] = steps[2][0].y - 40;
-                steps[2][0].body.velocity.y = -10;
+                obstacles[0].body.velocity.y = +10;
+                break;
+            case (2):
+                steps[0][0].body.velocity.y = +10;
+                steps[0][0].alpha = 0;
         }
     }
 }
@@ -72,13 +90,15 @@ function reset3()
     steps[0][0].body.velocity.y = 0;
     steps[0][1] = null;
 
-    steps[2][0].x = 1450;
-    steps[2][0].y = 475;
-    steps[2][0].body.velocity.y = 0;
-    steps[2][1] = null;
-
-    levers[0][1] = false;
     plates[0][1] = false;
+
+    obstacles[0].x = 200;
+    obstacles[0].y = 425;
+    obstacles[0].body.y = 0;
+    obstacles[1].x = 500;
+    obstacles[1].y = 400;
+    obstacles[2].x = 500;
+    obstacles[2].y = 600;
 
     player.x = 0;
     player.y = 0;
