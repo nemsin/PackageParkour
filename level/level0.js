b5221909989318ab5e30e10cabc21a7dbefd5bca
Jerrz <mail@jeremy-gross.de>
2018-01-28 10:41:42 +0100

    completed level 0

--- level/level0.js
+++ level/level0.js
@@ -2,26 +2,36 @@ function buildWorld0()
 {
     // Triggers
     levers[0] = [];
-    levers[0][0] = game.add.sprite(350, 425, 'lever');
+    levers[0][0] = game.add.sprite(525, 425, 'lever');
     levers[0][1] = false;
 
-    //plates[0] = [];
-    //plates[0][0] = game.add.sprite(200, 545, 'plate');
-    //plates[0][1] = false;
+    plates[0] = [];
+    plates[0][0] = game.add.sprite(1503, 398, 'plate');
+    plates[0][0].scale.setTo(3, 1);
+    plates[0][1] = false;
 
-    //liftable steps require physics!!
-    steps[1] = [];
-    steps[1][0] = grounds.create(250, 550, 'ground');
-    //steps[1][0].scale.setTo(0.1, 1);
-    game.physics.enable(steps[1][0], Phaser.Physics.ARCADE);
-
-    // Ground Test
+    // First step
     steps[0] = [];
-    steps[0][0] = grounds.create(300, 475, 'ground');
-    steps[0][0].scale.setTo(1, 1.5);
+    steps[0][0] = grounds.create(450, 550, 'ground');
+    //steps[1][0].scale.setTo(0.1, 1);
+    game.physics.enable(steps[0][0], Phaser.Physics.ARCADE);
+
+    steps[1] = [];
+    steps[1][0] = grounds.create(500, 475, 'ground');
+    steps[1][0].scale.setTo(1, 1.5);
+
+    // Second step
+    steps[2] = [];
+    steps[2][0] = grounds.create(1450, 475, 'ground');
+    steps[2][0].scale.setTo(1, 2);
+    game.physics.enable(steps[2][0], Phaser.Physics.ARCADE);
+
+    steps[3] = [];
+    steps[3][0] = grounds.create(1500, 400, 'ground');
+    steps[3][0].scale.setTo(1, 2);
 
     // Finish
-    finish = game.add.sprite(1000, 450, 'finish');
+    finish = game.add.sprite(2475, 375, 'finish');
     game.physics.enable(finish, Phaser.Physics.ARCADE);
 }
 
@@ -33,8 +43,8 @@ function leverHandler0()
         switch(triggerIndex)
         {
             case (0):
-                steps[1][1] = steps[1][0].y - 40;
-                steps[1][0].body.velocity.y = -10;
+                steps[0][1] = steps[0][0].y - 40;
+                steps[0][0].body.velocity.y = -10;
         }
     }
 }
@@ -47,7 +57,8 @@ function plateHandler0()
         switch(triggerIndex)
         {
             case (0):
-            // DO STUFF HERE
+                steps[2][1] = steps[2][0].y - 40;
+                steps[2][0].body.velocity.y = -10;
         }
     }
 }
@@ -70,7 +81,10 @@ function finishHandler0()
     level = 0;
     steps[0][0].destroy();
     steps[1][0].destroy();
+    steps[2][0].destroy();
+    steps[3][0].destroy();
     levers[0][0].destroy();
+    plates[0][0].destroy();
     player.x = 0;
     player.y = 0;
     player.body.velocity.x = 0;
@@ -82,12 +96,18 @@ function finishHandler0()
 
 function reset0()
 {
-    steps[1][0].x = 250;
-    steps[1][0].y = 550;
-    steps[1][0].body.velocity.y = 0;
-    steps[1][1] = null;
+    steps[0][0].x = 450;
+    steps[0][0].y = 550;
+    steps[0][0].body.velocity.y = 0;
+    steps[0][1] = null;
+
+    steps[2][0].x = 1450;
+    steps[2][0].y = 475;
+    steps[2][0].body.velocity.y = 0;
+    steps[2][1] = null;
 
     levers[0][1] = false;
+    plates[0][1] = false;
 
     player.x = 0;
     player.y = 0;
